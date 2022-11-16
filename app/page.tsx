5c327657f5f9247a7af272eecc9af745cd1d9906
Alex Toff <me@alextoff.uk>
2022-11-16 22:02:56 +0000

    Add devcontainer, segregate categories, change number format depending on amount

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,8 @@ import '../styles/globals.css'
 import { CatalogObject, Client, Environment, CatalogItem, CatalogImage } from "square";
 import Image from "next/image";
 
-const DISCONTINUED_CATEGORIES = ["E6BBDSBEAJJJKILQH2WQAFU6"];
+const DRINKS_CATEGORY = "HKY5KICRT6OKZB6MC2CTWVW6";
+const SNACKS_CATEGORY = "N726HTMVPHM5YZTYZMWJSO3Z"
 
 interface Item {
   id: string,
@@ -16,59 +17,50 @@ const priceFormatter = new Intl.NumberFormat('en-GB', {
 });
 
 function getFormattedPrice(item: CatalogItem): string | null {
-  const price = item.variations?.[0].itemVariationData?.priceMoney?.amount;
+  const price = Number(item.variations?.[0].itemVariationData?.priceMoney?.amount);
   if (price == null) return null;
-  return priceFormatter.format(Number(price) / 100);
+  if (price < 100) return `${price}p`
+  if (price % 100 == 0) return `£${price / 100}`
+  return priceFormatter.format(price / 100);
 }
 
-function getItemImage(item: CatalogItem, images: Map<string, CatalogObject>): CatalogImage | null {
-    const imageId = item.imageIds?.[0];
-    if (imageId == null) return null;
-    return images.get(imageId)?.imageData ?? null;
+function getItemImage(item: CatalogItem, images: CatalogObject[]): CatalogImage | null {
+  const imageId = item.imageIds?.[0];
+  if (imageId == null) return null;
+  return images.find(image => image.id == imageId)?.imageData ?? null;
 }
 
-/**
-* An item is only valid if:
-*  - It has a category 
-*  - It is not in a discontinued category
-*/
-function isValidItem(object: CatalogObject) {
-    return (
-      object.itemData!.categoryId != null 
-      && !DISCONTINUED_CATEGORIES.includes(object.itemData!.categoryId)
-    );
-}
-
-async function fetchCatalogItems(): Promise<Item[]> {
+async function fetchCatalogItems(categoryId: string): Promise<Item[]> {
   const client = new Client({
     accessToken: process.env.SQUARE_ACCESS_TOKEN,
     environment: Environment.Production,
   });
 
-  const listLocationsResponse = await client.catalogApi.listCatalog(undefined, "ITEM,IMAGE");
-  const objects = listLocationsResponse.result.objects ?? [];
+  const listLocationsResponse = await client.catalogApi.searchCatalogObjects({
+    objectTypes: ["ITEM"],
+    query: {
+      exactQuery: {
+        attributeName: "category_id",
+        attributeValue: categoryId
+      }
+    },
+    includeRelatedObjects: true
+  });
+  const items = listLocationsResponse.result.objects ?? [];
+  const relatedObjects = listLocationsResponse.result.relatedObjects ?? [];
 
 
-  type CatalogData = { images: Map<string, CatalogObject>, items: CatalogObject[] };
-  const { images, items }: CatalogData = objects.reduce((data, object) => {
-    if (object.type === "IMAGE") data.images.set(object.id, object)
-    else if (object.type === "ITEM") {
-        if (isValidItem(object)) {
-          data.items.push(object);
-        }
-    }
-    return data;
-  }, { images: new Map(), items: [] } as CatalogData)
+  const images = relatedObjects.filter(object => object.type == "IMAGE")
 
 
   return items.map(item => {
-      const itemData = item.itemData!;
-      const image = getItemImage(item.itemData!, images);
-      return {
-          id: item.id,
-          item: itemData,
-          image: image!,
-      } 
+    const itemData = item.itemData!;
+    const image = getItemImage(item.itemData!, images);
+    return {
+      id: item.id,
+      item: itemData,
+      image: image!,
+    }
   });
 }
 
@@ -78,25 +70,32 @@ function ItemCard(props: { item: Item }) {
     <div className="w-full relative">
       {
         item.image?.url != null && <div>
-          <Image className="rounded-t-lg" src={item.image!.url!} alt="product image" height={250} width={250}/>
+          <Image className="rounded-t-lg" src={item.image!.url!} alt="product image" height={250} width={250} />
         </div>
       }
       <div className="absolute top-0 bg-blue-500 text-white p-2 rounded text-3xl">
-          <div className="flex justify-between items-center">
-              {getFormattedPrice(item.item)}
-          </div>
+        <div className="flex justify-between items-center">
+          {getFormattedPrice(item.item)}
+        </div>
       </div>
     </div>
   );
 }
 
 export default async function Page() {
-  const items = await fetchCatalogItems();
+  const snackItems = await fetchCatalogItems(SNACKS_CATEGORY);
+  const drinkItems = await fetchCatalogItems(DRINKS_CATEGORY);
   return <div className='h-screen'>
-    <div className="grid grid-cols-1 sm:grid-cols-6 auto-cols-max">
-      { items.map(item => {
-          return <ItemCard key={item.id} item={item}/>;
-      }) }
+    <div className='flex'>
+      <div className="grid grid-cols-1 sm:grid-cols-6 auto-cols-max">
+        {snackItems.map(item => {
+          return <ItemCard key={item.id} item={item} />;
+        })}
+      </div><div className="grid grid-cols-1 sm:grid-cols-6 auto-cols-max">
+        {drinkItems.map(item => {
+          return <ItemCard key={item.id} item={item} />;
+        })}
+      </div>
     </div>
   </div>;
 }
